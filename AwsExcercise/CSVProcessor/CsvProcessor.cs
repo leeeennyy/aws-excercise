a3053cb6304cf751963a341305c686cbec0c2999
leeeennyy <leeeennyy@users.noreply.github.com>
2021-07-25 17:08:14 +1200

    Update CSV processor to try get the file from S3 bucket

--- AwsExcercise/CSVProcessor/CsvProcessor.cs
+++ AwsExcercise/CSVProcessor/CsvProcessor.cs
@@ -1,55 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
+using Domain.Models;
+using Domain.Readers;
 
 namespace CSVProcessor
 {
     public class CsvProcessor : ICsvProcessor
     {
-        private readonly IMeterRepository _meterRepository;
+        private readonly IMeterUsageCsvReader _meterUsageCsvReader;
 
-        public CsvProcessor()
+        public CsvProcessor(IMeterUsageCsvReader meterUsageCsvReader)
         {
+            _meterUsageCsvReader = meterUsageCsvReader;
         }
 
         public async Task ProcessFile(string filepath)
         {
-            List<MeterUsage> meterUsages = new();
-
-            filepath = @"C:\Dev\aws-excercise\consumption.csv";
-            using (StreamReader sr = new StreamReader(filepath))
-            {
-                string line = sr.ReadLine(); // (headers)
-                while ((line = sr.ReadLine()) != null)
+            //try
+            //{
+                List<MeterUsage> meterUsages = await _meterUsageCsvReader.GetMeterUsagesFromFile("consumption.csv");
+                foreach (var meterUsage in meterUsages)
                 {
-                    string[] meterUsageData = line.Split(',');
-
-                    Meter meter = new Meter
-                    {
-                        MeterId = new Guid(),
-                        MeterName = meterUsageData[0]
-                    };//await _meterRepository.GetMeterFromMeterName(meterUsageData[0]);
-                    DateTime date = DateTime.Parse(meterUsageData[1]);
-
-                    int timeSlice = 0;
-                    int totalUsage = 0;
-                    for (int i = 2; i < meterUsageData.Length; i++)
-                    {
-                        MeterUsage meterUsage = new MeterUsage
-                        {
-                            MeterId = meter.MeterId,
-                            DateTime = date.AddMinutes(timeSlice * 30),
-                            Usage = int.Parse(meterUsageData[i])
-                        };
-
-                        totalUsage += meterUsage.Usage;
-                        meterUsages.Add(meterUsage);
-                    }
-
-                    Console.WriteLine($"{meter} {date} {totalUsage}");
+                    Console.WriteLine($"{meterUsage.Meter} {meterUsage.DateTime} {meterUsage.Usage}");
                 }
-            }
+
+                // TODO: Store them into a database in cloud
+            //}
+            //catch (Exception ex)
+            //{
+            //    // TODO: Log error and mark the whole file as invalid
+            //}
+
+            //filepath = @"C:\Dev\aws-excercise\consumption.csv";
+            //using (StreamReader sr = new StreamReader(filepath))
+            //{
+            //    string line = sr.ReadLine(); // (headers)
+            //    while ((line = sr.ReadLine()) != null)
+            //    {
+            //        string[] meterUsageData = line.Split(',');
+            //        DateTime date = DateTime.Parse(meterUsageData[1]);
+
+            //        int timeSlice = 0;
+            //        int totalUsage = 0;
+            //        for (int i = 2; i < meterUsageData.Length; i++)
+            //        {
+            //            MeterUsage meterUsage = new MeterUsage
+            //            {
+            //                Meter = meterUsageData[0],
+            //                DateTime = date.AddMinutes(timeSlice * 30),
+            //                Usage = int.Parse(meterUsageData[i])
+            //            };
+
+            //            totalUsage += meterUsage.Usage;
+            //            meterUsages.Add(meterUsage);
+            //        }
+
+            //        Console.WriteLine($"{meterUsageData[0]} {date} {totalUsage}");
+            //    }
+            //}
         }
     }
 }
